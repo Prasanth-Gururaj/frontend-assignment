71d8c46cab8ee1baa15b3bb88c09f1f44ba4e1f8
Prasanth-Gururaj <79743886+Prasanth-Gururaj@users.noreply.github.com>
2021-10-03 15:11:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<input type="button" id="button" onmouseover="myFunction()" onclick="alert('clicked')">
+
+<script>
+// On mouse-over, execute myFunction
+function myFunction() {
+  document.getElementById("buttonk").click();
+}
+</script>
